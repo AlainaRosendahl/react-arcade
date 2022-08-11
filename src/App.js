4eabca7fa1337bb867e1747243388e67a6de15fa
Bryan Wilkerson <bwilk483@gmail.com>
2022-08-11 18:14:00 -0500

    nav bar finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import "./index.css";
+import Home from "./routes/Home";
+import Noah from "./routes/Noah";
+import Alaina from "./routes/Alaina";
+import Bryan from "./routes/Bryan";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="">React App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Noah" element={<Noah />}></Route>
+        <Route path="/Alaina" element={<Alaina />}></Route>
+        <Route path="/Bryan" element={<Bryan />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/Bryan.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Nav/Nav";
+
+const Bryan = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Bryan;
