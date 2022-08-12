38d358267319da982d43138a9381eeafc9ddf6a8
Bryan Wilkerson <bwilk483@gmail.com>
2022-08-11 20:09:31 -0500

    updated nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -20,7 +20,7 @@ const Navbar = () => {
 
   return (
     <div className={color ? "header header-bg" : "header"}>
-      <h1>Bryan Wilkerson</h1>
+      <h1>React Arcade</h1>
       <ul className={click ? "nav-menu-active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
