b117f567870df336c72f2a9677e7dc1878582b1a
LilCalc <noahsavinois@gmail.com>
2022-08-14 11:03:41 -0500

    Added working 2048 game to main page

--- client/src/routes/Noah/index.js
+++ client/src/routes/Noah/index.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
-import './App.css';
 import styled from "styled-components";
 import { Header, GameBoard } from './2048/components';
 import { BEST_SCORE_KEY } from "./2048/constants";
 import GlobalStyle from "./2048/styles/GlobalStyle";
 
-function App() {
+function Noah() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [reset, setReset] = useState(false);
@@ -18,16 +17,17 @@ function App() {
 
   return (
     <div className="App">
-      <GlobalStyle />
-      <Header score={score} bestScore={bestScore} reset={reset} setReset={setReset} />
-      <Main>
-        <GameBoard score={score} setScore={setScore} setBestScore={setBestScore} reset={reset} setReset={setReset}></GameBoard>
-      </Main>
-    </div >
-  );
+    <GlobalStyle />
+    <Header score={score} bestScore={bestScore} reset={reset} setReset={setReset} />
+    <Main>
+      <GameBoard score={score} setScore={setScore} setBestScore={setBestScore} reset={reset} setReset={setReset}></GameBoard>
+    </Main>
+  </div >
+   );
 }
 
-export default App;
+export default Noah;
+
 
 const Main = styled.main`
     display: flex;
