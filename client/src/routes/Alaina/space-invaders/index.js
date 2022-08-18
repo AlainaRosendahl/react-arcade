0f6ebfb846cdcfd467f4fd9b88a59973e64261fe
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-18 12:07:17 -0500

    finished space invaders

--- client/src/routes/Alaina/space-invaders/index.js
+++ client/src/routes/Alaina/space-invaders/index.js
@@ -1,6 +1,8 @@
-import EnemyController from "./EnemyController"
+import EnemyController from "./EnemyController.js";
+import Player from "./Player.js";
+import BulletController from "./BulletController.js";
 
-const canvas = document.getElementById("Game");
+const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 600;
@@ -9,13 +11,58 @@ canvas.height = 600;
 const background = new Image();
 background.src = "images/space.png";
 
-const enemyController = new EnemyController(canvas)
+const playerBulletController = new BulletController(canvas, 10, "red", true);
+const enemyBulletController = new BulletController(canvas, 4, "white", false);
+const enemyController = new EnemyController(
+  canvas,
+  enemyBulletController,
+  playerBulletController
+);
+const player = new Player(canvas, 3, playerBulletController);
 
-function Game(){
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    enemyController.Draw(ctx);
+let isGameOver = false;
+let didWin = false;
+
+function game() {
+  checkGameOver();
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  displayGameOver();
+  if (!isGameOver) {
+    enemyController.draw(ctx);
+    player.draw(ctx);
+    playerBulletController.draw(ctx);
+    enemyBulletController.draw(ctx);
+  }
 }
 
-setInterval(Game,1000 / 60);
+function displayGameOver() {
+  if (isGameOver) {
+    let text = didWin ? "You Win" : "Game Over";
+    let textOffset = didWin ? 3.5 : 5;
 
-module.export = Game;
+    ctx.fillStyle = "white";
+    ctx.font = "70px Arial";
+    ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
+  }
+}
+
+function checkGameOver() {
+  if (isGameOver) {
+    return;
+  }
+
+  if (enemyBulletController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if (enemyController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if (enemyController.enemyRows.length === 0) {
+    didWin = true;
+    isGameOver = true;
+  }
+}
+
+setInterval(game, 1000 / 60);
