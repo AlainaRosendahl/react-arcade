b8a9e2209bf4564e292ca2281b46b4a21ef66c65
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-18 11:52:29 -0500

    move down timers and direction

--- client/src/routes/Alaina/space-invaders/EnemyController.js
+++ client/src/routes/Alaina/space-invaders/EnemyController.js
@@ -10,27 +10,87 @@ export default class EnemyController {
         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
       ];
 
+      enemyRows = [];
+
+      currentDirection = MovingDirection.right;
+      xVelocity = 0;
+      yVelocity = 0;
+      defaultXVelocity = 1;
+      defaultYVelocity = 1;
+      moveDownTimerDefault = 30;
+      moveDownTimer = this.moveDownTimerDefault;
+      
     constructor(canvas){
         this.canvas = canvas;
         this.createEnemies();
     }
 
     draw(ctx) {
-        this.drawEnemies(ctx);
+    this.decrementMoveDownTimer();
+    this.updateVelocityAndDirection();
+    this.collisionDetection();
+    this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
     }
 
-    drawEnemies(ctx) {
-        this.enemyRows.flat().forEach((enemy)=>{
-            enemy.draw(ctx);
+    updateVelocityAndDirection() {
+        for (const enemyRow of this.enemyRows) {
+          if (this.currentDirection == MovingDirection.right) {
+            this.xVelocity = this.defaultXVelocity;
+            this.yVelocity = 0;
+            const rightMostEnemy = enemyRow[enemyRow.length - 1];
+            if (rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+              this.currentDirection = MovingDirection.downLeft;
+              break;
+            }
+          } else if (this.currentDirection === MovingDirection.downLeft) {
+            if (this.moveDown(MovingDirection.left)) {
+              break;
+            }
+          } else if (this.currentDirection === MovingDirection.left) {
+            this.xVelocity = -this.defaultXVelocity;
+            this.yVelocity = 0;
+            const leftMostEnemy = enemyRow[0];
+            if (leftMostEnemy.x <= 0) {
+              this.currentDirection = MovingDirection.downRight;
+              break;
+            }
+          } else if (this.currentDirection === MovingDirection.downRight) {
+            if (this.moveDown(MovingDirection.right)) {
+              break;
+            }
+          }
+        }
+      }
+    
+      moveDown(newDirection) {
+        this.xVelocity = 0;
+        this.yVelocity = this.defaultYVelocity;
+        if (this.moveDownTimer <= 0) {
+          this.currentDirection = newDirection;
+          return true;
+        }
+        return false;
+      }
+    
+      drawEnemies(ctx) {
+        this.enemyRows.flat().forEach((enemy) => {
+          enemy.move(this.xVelocity, this.yVelocity);
+          enemy.draw(ctx);
+        });
+      }
+    
+      happy = () => {};
+    
+      createEnemies() {
+        this.enemyMap.forEach((row, rowIndex) => {
+          this.enemyRows[rowIndex] = [];
+          row.forEach((enemyNubmer, enemyIndex) => {
+            if (enemyNubmer > 0) {
+              this.enemyRows[rowIndex].push(
+                new Enemy(enemyIndex * 50, rowIndex * 35, enemyNubmer)
+              );
+            }
+          });
         });
     }
-
-    createEnemies(){
-        this.enemyMap.forEach((row, rowIndex)=>{
-            this.enemyRows[rowIndex] = [];
-            row.forEach((enemyNumber, enemyIndex)=>{
-                this.enemyRows[rowIndex].push(new enemyIndex(enemyIndex* 50, rowIndex * 35, enemyNumber))
-            });
-        });
-    }
-}
