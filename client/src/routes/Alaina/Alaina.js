4950f4a66126fff5508fcdeec141f14b8e3bcc7f
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-18 19:31:14 -0500

    making memory game instead

--- client/src/routes/Alaina/Alaina.js
+++ client/src/routes/Alaina/Alaina.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "../../components/Nav/Nav";
-import Game from "../Alaina/space-invaders/index"
 
 const Alaina = () => {
   return (
     <div>
       <Navbar />
-      <Game />
     </div>
   );
 };
