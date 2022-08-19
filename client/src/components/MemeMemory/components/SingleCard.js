8548bd719c616c84fc40cfee828ca487a5c65711
Bryan Wilkerson <bwilk483@gmail.com>
2022-08-19 16:21:24 -0500

    memory image working

--- client/src/components/MemeMemory/components/SingleCard.js
+++ client/src/components/MemeMemory/components/SingleCard.js
@@ -1,14 +1,13 @@
-import './SingleCard.css'
-
+import "./SingleCard.css";
+import cover from "../../../assets/img/sad-cat.png";
 
 export default function SingleCard({ card }) {
-
-    return(
-        <div className="card">
-          <div>
-        <img className="front" src={card.src} alt="card front"></img>
-        <img className="back" src="../img/cover.png" alt="card back" />
-          </div>
-          </div>
-    )
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src="" alt="card front"></img>
+        <img className="back" src={cover} alt="card back" />
+      </div>
+    </div>
+  );
 }
