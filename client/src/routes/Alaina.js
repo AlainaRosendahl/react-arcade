0f0dd1502bdf9aa97ed9ddd86817976251c921ba
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-19 13:49:11 -0500

    card component created, not displaying with nav anymore

--- client/src/routes/Alaina.js
+++ client/src/routes/Alaina.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/Nav/Nav";
-import MemeMemory from "../components/memory-game/App"
+import MemeMemory from "../components/MemeMemory/App.js"
 
 const Alaina = () => {
   return (
