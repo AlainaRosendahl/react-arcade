78e5e0056b98bf137cacdacbb4bb8f0f3daaaa2e
Bryan Wilkerson <bwilk483@gmail.com>
2022-08-21 00:37:12 -0500

    did not work trying again on bryan 3

---
+++ client/src/routes/Noah.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Noah2 from "../components/Noah/index";
+import Navbar from "../components/Nav/Nav";
+
+const Noah = () => {
+  <div>
+    <Navbar />
+    {/* <Noah2 /> */}
+  </div>;
+};
+
+export default Noah;
