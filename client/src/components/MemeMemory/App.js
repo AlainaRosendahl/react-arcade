a0113d783c9e4d059c8a1b3ff14b0367a3d4380f
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-23 11:52:38 -0500

    code conflict

--- client/src/components/MemeMemory/App.js
+++ client/src/components/MemeMemory/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import React from "react";
-import SingleCard from "./components/SingleCard";
-import { useState } from "react";
-
+import './App.css'
+import React, { useEffect } from "react";
+import SingleCard from './components/SingleCard';
+import { useState } from 'react';
+// import sadcat from '../../assets/img/sad-cat.png'
 const cardImages = [
   { src: "/img/sad-cat.png", matched: false },
   { src: "/img/spongebob.png", matched: false },
@@ -20,29 +20,72 @@ const MemeMemory = () => {
   const [choiceTwo, setChoiceTwo] = useState(null)
   const [disabled, setDisabled] = useState(false)
   //shuffle cards
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
+    const shuffleCards= () =>{
+      const shuffledCards = [...cardImages, ...cardImages]
+      .sort(()=>Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+  
+      setChoiceOne(null)
+      setChoiceTwo(null)
+      setCards(shuffledCards)
+      setTurns(0)
+     }
 
-    setCards(shuffledCards);
-    setTurns(0);
-  };
+     //handle a choice
+     const handleChoice = (card) => {
+       choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+     }
 
-  console.log(cards, turns);
+    // compare two selected cards
+    useEffect (()=> {
+      if (choiceOne && choiceTwo) {
+        setDisabled(true)
+        if (choiceOne.src === choiceTwo.src) {
+          setCards(prevCards => {
+            return prevCards.map(card =>{
+              if (card.src === choiceOne.src) {
+                return {...card, matched: true}
+              } else {
+                return card
+              }
+            })
+          })
+          resetTurn()
+        } else {
+          setTimeout(() => resetTurn(), 1000)
+        }
+      }
+    }, [choiceOne, choiceTwo])
+console.log(cards)
+     //reset choices & increase turn
+     const resetTurn = () => {
+       setChoiceOne(null)
+       setChoiceTwo(null)
+       setTurns(prevturns => prevturns + 1)
+       setDisabled(false)
+     }
+// starts new game automatically
+     useEffect(()=> {
+      shuffleCards()
+     }, [])
 
   return (
     <div className="App">
       <h1>Meme Match</h1>
       <button onClick={shuffleCards}>New Game</button>
-      <div className="card-grid">
-        {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
-        ))}
+      <div className= "card-grid">
+        {cards.map(card =>(
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice} 
+          flipped={card === choiceOne || card === choiceTwo || card.matched} 
+          disabled={disabled}/>
+          ))}
       </div>
       <p>Turns: {turns}</p>
     </div>
   );
 };
 
-export default MemeMemory;
+ export default MemeMemory;
