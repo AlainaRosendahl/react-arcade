2a80a22635f48bd70bb91b8ae5cc398274a20af4
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-24 12:04:17 -0500

    description

--- client/src/components/Home/HomeMemory.js
+++ client/src/components/Home/HomeMemory.js
@@ -16,19 +16,8 @@ const HomeMemory = () => {
           <div className="info-container">
             <h1>Meme Memory</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-              laudantium molestias eos sapiente officiis modi at sunt excepturi
-              expedita sint? Sed quibusdam recusandae alias error harum maxime
-              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
-              Officiis iure rerum voluptates a cumque velit{" "}
+              Meme Match! A simple game that matchs meme's with the click of a button. Built using react, this app has improved my development skills 
+              using mapping and useState. 
             </p>
             <button type="button" class="btn btn-primary" onClick="">
               Meme Memory
